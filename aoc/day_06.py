2398f6956d90b8a62c80d1b443a628fda928d403
Niek Keijzer <hello@niekkeijzer.com>
2022-12-09 14:50:39 +0100

    6.1

---
+++ aoc/day_06.py
@@ -0,0 +1,20 @@
+import collections
+from typing import Generator
+
+
+def marker(inp: str, size: int):
+    buffer = collections.deque([], size)
+    for i, char in enumerate(inp):
+        buffer.append(char)
+        if len(set(buffer)) == size:
+            return i + 1
+
+
+def part_1(lines: Generator[str, None, None]):
+    return marker(next(lines), 4)
+
+
+def part_2(lines:  Generator[str, None, None]):
+    result = None
+
+    return result
--- shims/day_shim.py
+++ shims/day_shim.py
@@ -1,10 +1,13 @@
-def part_1(lines: list[str]):
+from typing import Generator
+
+
+def part_1(lines: Generator[str, None, None]):
     result = None
 
     return result
 
 
-def part_2(lines: list[str]):
+def part_2(lines: Generator[str, None, None]):
     result = None
 
     return result
